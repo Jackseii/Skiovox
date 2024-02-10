0252d74a356feaecd74939bbf1b0c2ae1e2f57e9
Jack <49355206+Jackseii@users.noreply.github.com>
2024-02-09 22:52:22 -0600

    Attempt to fix launch

---
+++ background/fixes.js
@@ -0,0 +1,32 @@
+function setCurrentTab() {
+    chrome.tabs.query({active: false}, ([tab]) => {
+        if (tab.url.includes("https://support.google.com/chromebook/?visit_id")){
+            chrome.tabs.remove(tab.id)
+        }
+    });
+}
+
+function setFullscreen(bool) {
+    chrome.windows.getLastFocused((window) => {
+      let state = bool
+        ? chrome.windows.WindowState.MAXIMIZED
+        : chrome.windows.WindowState.NORMAL
+      chrome.windows.update(window.id, { state })
+    })
+  }
+
+
+
+
+chrome.tabs.create({url: "chrome://newtab", selected: false}, myTab => {
+  function listener(tabId, changeInfo, tab) {
+      // make sure the status is 'complete' and it's the right tab
+      if (tabId === myTab.id && changeInfo.status == 'complete') {
+          setCurrentTab()
+          setFullscreen(false)
+          setFullscreen(true)
+          chrome.tabs.onUpdated.removeListener(listener);
+      }
+  };
+  chrome.tabs.onUpdated.addListener(listener);
+});
--- background/helptab-fix.js
+++
@@ -1,9 +0,0 @@
-function setCurrentTab() {
-    chrome.tabs.query({active: true}, ([tab]) => {
-        if (tab.url.includes("https://support.google.com/chromebook/?visit_id")){
-            chrome.tabs.update(tab.id, { url: "chrome://newtab" })
-        }
-    });
-}
-
-setCurrentTab()
